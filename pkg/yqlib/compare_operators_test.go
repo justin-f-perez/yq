321175a8b707fcbb6b7c2b0ab0428c70898cf094
Mike Farah <mikefarah@gmail.com>
2022-03-18 13:22:51 +1100

    Added support for more comparison ops

--- pkg/yqlib/compare_operators_test.go
+++ pkg/yqlib/compare_operators_test.go
@@ -222,10 +222,142 @@ var compareOperatorScenarios = []expressionScenario{
 	},
 
 	// strings, not equal
+	{
+		description:    "Compare strings",
+		subdescription: "Compares strings by their bytecode.",
+		document:       "a: zoo\nb: apple",
+		expression:     ".a > .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: zoo\nb: apple",
+		expression: ".a < .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: zoo\nb: apple",
+		expression: ".a >= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: zoo\nb: apple",
+		expression: ".a <= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+
 	// strings, equal
+	{
+		skipDoc:    true,
+		document:   "a: cat\nb: cat",
+		expression: ".a > .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: cat\nb: cat",
+		expression: ".a < .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: cat\nb: cat",
+		expression: ".a >= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: cat\nb: cat",
+		expression: ".a <= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
 
 	// datetime, not equal
+	{
+		description:    "Compare date times",
+		subdescription: "You can compare date times. Assumes RFC3339 date time format, see [date-time operators](https://mikefarah.gitbook.io/yq/operators/date-time-operators) for more information.",
+		document:       "a: 2021-01-01T03:10:00Z\nb: 2020-01-01T03:10:00Z",
+		expression:     ".a > .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2020-01-01T03:10:00Z",
+		expression: ".a < .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2020-01-01T03:10:00Z",
+		expression: ".a >= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2020-01-01T03:10:00Z",
+		expression: ".a <= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+
 	// datetime, equal
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2021-01-01T03:10:00Z",
+		expression: ".a > .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2021-01-01T03:10:00Z",
+		expression: ".a < .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::false\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2021-01-01T03:10:00Z",
+		expression: ".a >= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
+	{
+		skipDoc:    true,
+		document:   "a: 2021-01-01T03:10:00Z\nb: 2021-01-01T03:10:00Z",
+		expression: ".a <= .b",
+		expected: []string{
+			"D0, P[a], (!!bool)::true\n",
+		},
+	},
 }
 
 func TestCompareOperatorScenarios(t *testing.T) {
